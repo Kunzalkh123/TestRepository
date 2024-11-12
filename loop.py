246e3da842799d1b557d205bc9b0a903e4e71383
Kunzal Khawaja Fahim <kunzalfahim04@gmail.com>
2024-11-12 13:47:51 +0400

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,23 @@
+for i in range(1, 11):
+    print(i)
+
+total = 0
+for i in range(1, 6):
+    total += i
+print("Sum:", total)
+
+text = input("Enter a string: ")
+for char in text:
+    print(char)
+
+number = int(input("Enter a number: "))
+while number >= 0:
+    print(number)
+    number -= 1
+
+num = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+for i in range (1,11):
+    print(i)
